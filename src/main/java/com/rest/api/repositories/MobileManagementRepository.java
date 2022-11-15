079ddae69b3e5d5b45f42bf10815059b228d5d73
Rahul <pantrahul02@gmail.com>
2022-11-15 08:59:34 +0530

    unused code removed

--- src/main/java/com/rest/api/repositories/MobileManagementRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rest.api.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rest.api.entities.MobileManagement;
-public interface MobileManagementRepository extends JpaRepository<MobileManagement, Long> {
-}
